c4dbf4558fcb489ed8005732696a5a10381cdf82
Alina Bezuhla <alinabezuglaya123@gmail.com>
2023-08-05 22:19:44 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function toReadable (number) {
-  
+    let readableStr = ""
+    let first_twenty = [ "zero", "one", "two", "three", 
+                        "four", "five", "six", 
+                        "seven", "eight", "nine", 
+                        "ten", "eleven", "twelve",
+                        "thirteen", "fourteen", "fifteen",
+                        "sixteen", "seventeen", "eighteen",
+                        "nineteen"]
+
+    let tens = ["", "", "twenty", "thirty", "forty",
+                "fifty", "sixty", "seventy",
+                "eighty", "ninety"]
+
+    if (Math.floor(number/100) > 0 && number >= 100) {
+        readableStr = first_twenty[Math.floor(number/100)] + " hundred"
+        number = number - Math.floor(number/100) * 100
+        
+        if (number >= 1 && number <= 19) {
+            readableStr += " " + first_twenty[number]
+        }
+        else if (Math.floor(number/10) > 0) {
+            readableStr += " " + tens[Math.floor(number/10)]
+            number = number - Math.floor(number/10) * 10
+            if (number >= 1 && number <= 9) {
+                readableStr += " " + first_twenty[number]
+            }
+        }
+    }
+    else {
+        if (number >= 0 && number <= 19) {
+            readableStr = first_twenty[number]
+        }
+        else if (Math.floor(number/10) > 0) {
+            readableStr = tens[Math.floor(number/10)]
+            number = number - Math.floor(number/10) * 10
+            if (number >= 1 && number <= 9) {
+                readableStr += " " + first_twenty[number]
+            }
+        }
+    }
+    return readableStr;
 }
